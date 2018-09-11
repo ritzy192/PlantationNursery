4e42f17aaeb2741f093d2048a106b192a0548597
Abhay Singh <egabhay@gmail.com>
2018-09-11 19:16:20 +0530

    Added Nursery Folder in WP-CONTENT

---
+++ SinghPlantation/wp-content/themes/nursery/assets/js/backbone.js
@@ -0,0 +1,9 @@
+//	#Scroll To Top
+//	==================================================
+
+	$(document).ready(function() {
+		$('.scroll-to-top').click(function() {
+			$('html, body').animate({ scrollTop: 0 }, 1600, 'easeInOutQuart');
+			return false;
+		});
+	});
